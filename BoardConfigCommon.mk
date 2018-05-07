9bb251aeb706efbd7da03ae3573ae8dfe77d5d51
chakaponden <motometalor@gmail.com>
2018-05-07 12:26:06 +0300

    qcom-common: remove softfp neon-vfpv4 flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,8 +34,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Wifi related defines
